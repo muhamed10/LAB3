12b73f349fa23fd41b6c909d47e5e64c54a22ba2
Anton§ <gurkor211@gmail.com>
2020-02-09 17:16:26 +0100

    modified:   DropBrick.java

--- DropBrick.java
+++ DropBrick.java
@@ -2,20 +2,17 @@ package LAB3;
 import java.awt.event.MouseEvent;
 
 public class DropBrick extends Brick {
-<<<<<<< HEAD
+
 	public static final int WIDTH = 50;
 	public static final int HEIGHT = 25;
 	
 	public DropBrick(int x, int y) {
 		super(x, y);
-=======
-
-	public DropBrick(int x, int y) {
-		super(x, y);
 	}
+
 	
 	public void mouseClicked(MouseEvent e) {
 		this.setLocation(this.getXLocation(), 400);
->>>>>>> 799c4ce10fb773d76539026874c952821f13a047
+
 	}
 }
