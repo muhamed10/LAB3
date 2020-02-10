993dcbfe69694b9524b67649f8eb7a9128bdeafb
muhamed10 <59508281+muhamed10@users.noreply.github.com>
2020-02-10 10:35:10 +0100

    wef

--- Game.java
+++ Game.java
@@ -10,15 +10,20 @@ public class Game extends Frame implements Updater{
 	private MoveTimer _timer;
 	private Ball _ball, _2ball;
 	private Bat _bat;
+	private Brick [][] _brick;
 	public Game()
+	
 	{
 		_ball = new Ball(0,0);
 		_2ball = new Ball(100, 100);
-		
 		_timer = new MoveTimer(INTERVAL, this);
 		_timer.start();
-		
 		_bat = new Bat(300,450);
+		for (int i=0;i<5;i++) {
+			for (int j=0;j<3;j++) {
+				
+			}
+		}
 	}
 	
 	public static void main(String[] args)
